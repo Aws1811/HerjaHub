8fed58cb749e2240528fbcba4f37bb866d6a1358
Aws1811 <awssleebi181@gmail.com>
2026-07-18 14:01:19 +0300

    style desgin test for products details

--- HerjaHub/herjahub/src/main/java/com/axsos/project/repositores/ProductRepository.java
+++ HerjaHub/herjahub/src/main/java/com/axsos/project/repositores/ProductRepository.java
@@ -1,10 +1,15 @@
 package com.axsos.project.repositores;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.axsos.project.models.Product;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    List<Product> findAll();
+    // Get products that are still available.
+    List<Product> findByQuantityGreaterThan(Integer quantity);
 }
